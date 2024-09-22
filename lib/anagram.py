754180969b8b5c075ee8b7f4b48df5d9b0b2321d
melilei <melilei6784@gmail.com>
2024-09-22 15:34:25 +0300

    completed assingment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        anagrams = []
+        for w in word_list:
+            if self.is_anagram(w):
+                anagrams.append(w)
+        return anagrams
+
+    def is_anagram(self, word):
+        word = word.lower()
+        if len(word) != len(self.word):
+            return False   
+        return sorted(word) == sorted(self.word)
